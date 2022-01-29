be8925ceda027b66b268f1005fd14a9fe8bf1b41
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-01-28 20:37:04 -0600

    3-2 descargando info y creando nodos

--- src/index.js
+++ src/index.js
@@ -3,4 +3,33 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//web API
+//Conectar al server
+window
+    .fetch(url)
+    .then((respuesta) => respuesta.json())
+    //procesar y convertir en json
+    .then((responseJson) => {
+        const todosLosItems = [];
+        responseJson.data.forEach((item) => {
+
+            //crear img
+            const imagen = document.createElement('img');
+
+            // crear titulo
+            const title = document.createElement("h2");
+
+            // crear precio
+            const price = document.createElement("div");
+
+            const container = document.createElement("div");
+            container.append(imagen, title, price);
+
+            todosLosItems.push(container);
+
+        });
+
+        document.body.append(...todosLosItems);
+    });
