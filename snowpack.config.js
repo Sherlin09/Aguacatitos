0a79be0e8f887af12a2487a080a15f3f9dc88c00
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-01-28 22:13:36 -0600

    correcion

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-    public: '/',
-    src: '/_dist_',
-  },
-  buildOptions: {
-    baseUrl: '',
-  },
+    mount: {
+        public: '/',
+        src: '/_dist_',
+    },
+    buildOptions: {
+        baseUrl: 'https://github.com/Sherlin09/Aguacatitos.git',
+    },
 }
